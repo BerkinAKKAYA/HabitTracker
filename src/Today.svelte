f88c8a0e87d35a064c11e0f0480e6ba8b1806203
berkinakkaya <berkin_akkaya@hotmail.com>
2020-10-23 22:51:14 +0300

    Add Animated Checkboxes

--- src/Today.svelte
+++ src/Today.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Checkbox from "./Checkbox.svelte";
+
 	export let database = {};
 	export let currentDate = "01-2000";
 
@@ -13,32 +15,29 @@
 			today.push([data, monthData.includes(date)]);
 		}
 		today = today;
-
-		console.log(today);
 	}
 
 	function Toggle(habit) {
-		const dt = currentDate;
-		const td = new Date().getDate().toString();
+		const today = new Date().getDate().toString();
+		let data = database[habit][currentDate];
 
-		if (database[habit][dt].includes(td)) {
-			database[habit][dt] = database[habit][dt].filter((x) => x != td);
+		if (data.includes(today)) {
+			data = data.filter((x) => x != today);
 		} else {
-			database[habit][dt] = [...database[habit][dt], td];
+			data = [...data, today];
 		}
+
+		database[habit][currentDate] = data;
 	}
 </script>
 
 <main>
 	<h2>TODAY</h2>
 	{#each today as habit}
-		<div class="habit" class:done={habit[1]}>
-			<input
-				type="checkbox"
-				on:click={() => Toggle(habit[0])}
-				checked={habit[1]} />
-			<span>{habit[0]}</span>
-		</div>
+		<Checkbox
+			task={habit[0]}
+			bind:checked={habit[1]}
+			onToggle={() => Toggle(habit[0])} />
 	{/each}
 </main>
 
@@ -47,34 +46,8 @@
 		margin-bottom: 50px;
 
 		h2 {
-			margin-bottom: 10px;
+			margin-bottom: 15px;
 			text-align: center;
-		}
-
-		.habit {
-			position: relative;
-			display: grid;
-			grid-template-columns: 14px 1fr;
-			gap: 10px;
-		}
-		.habit::after {
-			content: "";
-			display: block;
-			width: 0;
-			height: 1px;
-			background-color: black;
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			transition: all 0.3s ease;
-		}
-		.habit.done {
-			opacity: 0.5;
-
-			&::after {
-				width: calc(100% + 20px);
-			}
 		}
 	}
 </style>
